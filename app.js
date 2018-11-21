c6bc192b7f51b011a95da0f21157d24b46461fc5
MuhammadMullah <muhammadmullah16@gmail.com>
2018-11-21 14:32:37 +0300

    sending responses

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text-html');
+    // write data to the response
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from my node.js Server</h1></body>');
+    res.write('</html>');
+    res.end();
 });
 
 const port = 3000
