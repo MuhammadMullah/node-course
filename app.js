5af0db42c251ef68859f02ef68e46e201e3ec4c2
MuhammadMullah <muhammadmullah16@gmail.com>
2018-11-21 14:40:18 +0300

    routing request

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text-html');
+    const url = req.url
+
+    if (url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My First Page</title></head>');
+        res.write('<body><form action="message" method="POST"><input type="text" name="message"><button>Send</button></input></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
     // write data to the response
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
