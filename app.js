e53196de340e47898c737aa1e9a4ec60131149d1
MuhammadMullah <muhammadmullah16@gmail.com>
2018-11-24 12:11:25 +0300

    parsing incoming url

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
-app.use((req, res, next) => {
-    res.send('<h1>Hello from Express</h1>');
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<form action="product", method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
 });
+
+app.use('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/')
+});
+
+app.use('/', (req, res, next) => {
+    res.send("Hello from Express");
+});
+
 
 const port = 3000
 // const hostName = 'localhost'
