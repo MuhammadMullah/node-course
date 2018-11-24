6cb362b3a56cf258cd630619a40da014ba85a8d4
MuhammadMullah <muhammadmullah16@gmail.com>
2018-11-24 14:49:07 +0300

    finished section 5

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-const http = require('http');
 const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const app = express();
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
-const app = express();
-
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -19,6 +18,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-const port = 3000
-// const hostName = 'localhost'
-app.listen(port);
+app.listen(3000);
